c9b55c06ed95b0aed3db5e8d839bbf37643298d7
Andreiv Maksym <maxoggyha@gmail.com>
2025-02-25 01:16:28 +0900

    Add MobNet model

---
+++ Task 2/src/classification/class_model.py
@@ -0,0 +1,20 @@
+from tensorflow.keras.layers import Dense, Dropout
+from dataset_loader import train_generator
+
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import GlobalAveragePooling2D
+
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.5)(x)
+output = Dense(len(train_generator.class_indices), activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=output)
+
+# Freeze the base model layers
+for layer in base_model.layers:
+    layer.trainable = False
