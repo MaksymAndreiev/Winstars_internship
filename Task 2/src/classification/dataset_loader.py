490826044b2ff56a5a982b12f9958a422316cf94
Andreiv Maksym <maxoggyha@gmail.com>
2025-02-24 14:58:58 +0900

    Add image and data generators

---
+++ Task 2/src/classification/dataset_loader.py
@@ -0,0 +1,43 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_dir = '../../data/train'
+val_dir = '../../data/valid'
+test_dir = '../../data/test'
+
+img_size = 224
+batch_size = 32
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+# Load data
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode='categorical'
+)
+
+val_generator = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode='categorical'
+)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode='categorical'
+)
