53a32cf032c796ea6b2eb37e3b8c70e529f5113f
Andreiv Maksym <maxoggyha@gmail.com>
2025-02-25 03:15:12 +0900

    Refactor the code

--- Task 2/ner/inference_ner.py
+++ Task 2/ner/inference_ner.py
@@ -1,7 +1,8 @@
 import spacy
+import os
 
 # Load the trained NER model
-model = spacy.load("../models/ner_model")
+model = spacy.load(os.path.join(os.path.dirname(__file__), "../models/ner_model"))
 
 def get_entities(text):
     """
@@ -15,27 +16,28 @@ def get_entities(text):
         entities.append({"entity": ent.text, "label": ent.label_})
     return entities
 
-# Example sentences
-example_sentences = [
-    "A dog was resting near the lake.",
-    "Have you ever seen a tiger in the wild?",
-    "The elephant at the zoo was very active.",
-    "My friend has a pet cat at home.",
-    "The baby seal stayed close to its mother.",
-    "Scientists are studying the behavior of the dolphin.",
-    "The bear made a loud noise at night.",
-    "In some cultures, owl is considered sacred.",
-    "A lion was spotted near the river.",
-    "Children love to draw pictures of horse.",
-    "The rabbit was searching for food in the grass.",
-    "A squirrel approached cautiously, sensing danger.",
-    "I guess it's a fox.",
-    "I see a wolf in the image.",
-    "Gepard",
-    "This is a flamingo.",
-]
+if __name__ == "__main__":
+    # Example sentences
+    example_sentences = [
+        "A dog was resting near the lake.",
+        "Have you ever seen a tiger in the wild?",
+        "The elephant at the zoo was very active.",
+        "My friend has a pet cat at home.",
+        "The baby seal stayed close to its mother.",
+        "Scientists are studying the behavior of the dolphin.",
+        "The bear made a loud noise at night.",
+        "In some cultures, owl is considered sacred.",
+        "A lion was spotted near the river.",
+        "Children love to draw pictures of horse.",
+        "The rabbit was searching for food in the grass.",
+        "A squirrel approached cautiously, sensing danger.",
+        "I guess it's a fox.",
+        "I see a wolf in the image.",
+        "Gepard",
+        "This is a flamingo.",
+    ]
 
-# Process each sentence and extract entities
-for number, sentence in enumerate(example_sentences, 1):
-    entities = get_entities(sentence)
-    print(f"{number}. {sentence}: {entities}")
+    # Process each sentence and extract entities
+    for number, sentence in enumerate(example_sentences, 1):
+        entities = get_entities(sentence)
+        print(f"{number}. {sentence}: {entities}")
