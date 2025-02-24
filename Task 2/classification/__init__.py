4d5d8de05b3979f5e49f79e01736a121fdb1ee8a
Andreiv Maksym <maxoggyha@gmail.com>
2025-02-25 01:44:05 +0900

    Add __init__.py

---
+++ Task 2/classification/__init__.py
@@ -0,0 +1,3 @@
+from  .class_model import model as classification_model
+from .dataset_loader import train_generator, val_generator, test_generator
+from  .inference_class import get_random_image, predict
