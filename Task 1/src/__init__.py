7da092f2483bfa916281e9d4cb3bd7b99680d6d0
Andreiv Maksym <maxoggyha@gmail.com>
2025-05-22 11:34:39 +0900

    Refactor project structure and update README.md with detailed problem statement and approach

--- Task 1/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .random_forest import RandomForestClassifier
-from .feed_forward_nn import FeedForwardNeuralNetwork
-from .cnn import ConvolutionalNeuralNetwork
-from .mnist_classifier import MnistClassifier
-from .model_interface import MnistClassifierInterface
